76637e08eda9b27618b12c894f22296b8b82c16b
root <enidaucl@gmail.com>
2023-08-23 14:18:01 -0700

    check new main

--- shell.c
+++ shell.c
@@ -6,41 +6,64 @@
  * @envp: Array of env variables
  * Return: Return code of the command execution
  */
-int main(__attribute__((unused))int argc, char *argv[], char *envp[])
+
+int main(int argc, char *argv[], char *envp[])
 {
 	char *line, *command_path;
 	char **commands;
 	int status = 1;
 
-	while (status)
+	if (argc > 1)
 	{
-		status = isatty(0);
-		if (status == 1)
-			printf("#prompt$ ");
 		line = read_line();
 		if (line == NULL)
-			continue;
+			return (1);
 		commands = split_line(line);
 		if (commands[0])
 		{
-			if (strcmp(commands[0], "exit") == 0)
-				status = 0;
+			command_path = get_command_path(commands[0]);
+			if (command_path == NULL)
+				printf("%s: Command not found\n", commands[0]);
 			else
 			{
-				command_path = get_command_path(commands[0]);
-				if (command_path == NULL)
-					printf("%s: No such file or directory\n", argv[0]);
-				else
-				{
-					execute(command_path, commands, envp);
-					if (command_path != commands[0])
-						free(command_path);
-				}
+				execute(command_path, commands, envp);
+				free(command_path);
 			}
 		}
 		free(commands);
 		free(line);
-		line = NULL;
+	}
+	else
+	{
+		while (status)
+		{
+			if (isatty(STDIN_FILENO))
+				printf("#prompt$ ");
+			line = read_line();
+			if (line == NULL)
+				continue;
+			commands = split_line(line);
+			if (commands[0])
+			{
+				if (strcmp(commands[0], "exit") == 0)
+					status = 0;
+				else
+				{
+					command_path = get_command_path(commands[0]);
+					if (command_path == NULL)
+						printf("%s: Command not found\n", argv[0]);
+					else
+					{
+						execute(command_path, commands, envp);
+						if (command_path != commands[0])
+							free(command_path);
+					}
+				}
+			}
+			free(commands);
+			free(line);
+			line = NULL;
+		}
 	}
 	return (0);
 }
