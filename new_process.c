ef704c9234d2b47d87e2a20959ac6615e3976c16
root <enidaucl@gmail.com>
2023-08-24 05:07:29 -0700

    fork new_process

--- new_process.c
+++ new_process.c
@@ -3,29 +3,29 @@
 /**
  * execute - Function thar execute a command
  * @command_path: Path to the command to execute
- * @args: Arguments for the command
+ * @argv: Arguments for the command
  * @envp: Array of env variables
  * Return: Return 1 on successful execution
  */
 int execute(char *command_path, char *argv[], char *envp[])
 {
-  pid_t pid;
+	pid_t pid;
 
-  pid = fork();
-  if (pid == 0)
-    {
-      execve(command_path, argv, envp);
+	pid = fork();
+	if (pid == 0)
+	{
+		execve(command_path, argv, envp);
 
-      exit(1);
-    }
-  else if (pid > 0)
-    {
-      wait(NULL);
-    }
-  else
-    {
-      perror("Fork Failed");
-      exit(EXIT_FAILURE);
-    }
-  return (1);
+		exit(1);
+	}
+	else if (pid > 0)
+	{
+		wait(NULL);
+	}
+	else
+	{
+		perror("Fork Failed");
+		exit(EXIT_FAILURE);
+	}
+	return (1);
 }
