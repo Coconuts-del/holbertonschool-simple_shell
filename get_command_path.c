803c49c167992228ca4ef6210167ca3c12d31079
root <enidaucl@gmail.com>
2023-08-24 05:03:05 -0700

    command_path

--- get_command_path.c
+++ get_command_path.c
@@ -7,27 +7,28 @@
  */
 char *get_command_path(char *path)
 {
-  char *command_path = NULL;
-  if ((path[0] == '/') || (path [0] != '/'))
-    {
-      if (access(path, X_OK) == 0)
-	{
-	  command_path = path;
-	}
-      else
-	{
-	  char bin_path[] = "/bin/";
+	char *command_path = NULL;
 
-	  command_path = malloc(strlen(bin_path) + strlen(path) + 1);
-	  strcpy(command_path, bin_path);
-	  strcat(command_path, path);
-	  if (access(command_path, X_OK) != 0)
-	    {
-	      free(command_path);
-	      command_path = NULL;
-	    }
+	if ((path[0] == '/') || (path[0] != '/'))
+	{
+		if (access(path, X_OK) == 0)
+		{
+			command_path = path;
+		}
+		else
+		{
+			char bin_path[] = "/bin/";
 
+			command_path = malloc(strlen(bin_path) + strlen(path) + 1);
+			strcpy(command_path, bin_path);
+			strcat(command_path, path);
+			if (access(command_path, X_OK) != 0)
+			{
+				free(command_path);
+				command_path = NULL;
+			}
+
+		}
 	}
-    }
-  return (command_path);
+	return (command_path);
 }
