1df4fe45f290c0a14a8274d0416253e96e92426c
idoudi2020 <idoudimokhtar@gmail.com>
2023-08-24 03:38:52 -0700

    get

--- get_command_path.c
+++ get_command_path.c
@@ -1,46 +1,33 @@
 #include "shell.h"
-
 /**
  * get_command_path - Function to get command path
- * @command: Command name to look for
+ * @path: Path to look for
  * Return: Command path if found, NULL if the command is not found
+ *
  */
-char *get_command_path(char *command)
+char *get_command_path(char *path)
 {
-	char *command_path = NULL;
-	char *path_env = getenv("PATH");
-	char bin_path[SIZE];
-	char *path_copy = NULL;
-	char *token = NULL;
-
-	if (command[0] == '/')  /* Use == instead of !=  */
-		if (access(command, X_OK) == 0)
-			command_path = command;
-
-	if (path_env != NULL)
+  char *command_path = NULL;
+  if ((path[0] == '/') || (path [0] != '/'))
+    {
+      if (access(path, X_OK) == 0)
 	{
-		path_copy = strdup(path_env);
-		token = strtok(path_copy, ":");
-
-		while (token != NULL && command_path == NULL)
-		{
-			strcpy(bin_path, token);
-			command_path = malloc(strlen(bin_path) + strlen(command) + 2);
-			if (command_path != NULL)
-			{
-				strcat(bin_path, "/");
-				strcat(bin_path, command);
-				if (access(bin_path, X_OK) == 0)
-					strcpy(command_path, bin_path);
-				else
-				{
-					free(command_path);
-					command_path = NULL;
-				}
-			}
-			token = strtok(NULL, ":");
-		}
-		free(path_copy);
+	  command_path = path;
 	}
-	return (command_path);
+      else
+	{
+	  char bin_path[] = "/bin/";
+
+	  command_path = malloc(strlen(bin_path) + strlen(path) + 1);
+	  strcpy(command_path, bin_path);
+	  strcat(command_path, path);
+	  if (access(command_path, X_OK) != 0)
+	    {
+	      free(command_path);
+	      command_path = NULL;
+	    }
+
+	}
+    }
+  return (command_path);
 }
