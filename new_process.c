b7336542c9087dab1814b2f723d216c387dd0db5
idoudi2020 <idoudimokhtar@gmail.com>
2023-08-24 03:36:18 -0700

    new pro

--- new_process.c
+++ new_process.c
@@ -1,31 +1,31 @@
 #include "shell.h"
 
 /**
- * execute - Function that execute a command
+ * execute - Function thar execute a command
  * @command_path: Path to the command to execute
- * @argv: Arguments for the command
+ * @args: Arguments for the command
  * @envp: Array of env variables
  * Return: Return 1 on successful execution
  */
 int execute(char *command_path, char *argv[], char *envp[])
 {
-	pid_t pid;
-	int status;
+  pid_t pid;
 
-	pid = fork();
-	if (pid == 0)
-	{
-		execve(command_path, argv, envp);
-		exit(1);
-	}
-	else if (pid > 0)
-	{
-		wait(&status);
-	}
-	else
-	{
-		perror("Fork Failed");
-		exit(EXIT_FAILURE);
-	}
-	return (1);
+  pid = fork();
+  if (pid == 0)
+    {
+      execve(command_path, argv, envp);
+
+      exit(1);
+    }
+  else if (pid > 0)
+    {
+      wait(NULL);
+    }
+  else
+    {
+      perror("Fork Failed");
+      exit(EXIT_FAILURE);
+    }
+  return (1);
 }
