2653a0d4ea936d1afdf41e517d3e2647d4d650b4
root <enidaucl@gmail.com>
2023-08-24 05:12:50 -0700

    read_line

--- read_line.c
+++ read_line.c
@@ -2,34 +2,22 @@
 /**
  * read_line - Function to read a line from stdin
  *
- * Return: Return line as strinf
+ * Return: Return line
  *
  */
 char *read_line()
 {
-  char *line = NULL;
-  size_t buffer = 0;
+	char *line = NULL;
+	size_t size_line = 0;
+	int nread;
 
-  if (getline(&line, &buffer, stdin) == -1)
-    {
-      if (feof(stdin))
+	nread  =  getline(&line, &size_line, stdin);
+	if (nread == -1)
 	{
-	  if (isatty(STDIN_FILENO))
-	    {
-	      printf("\n");
-	    }
-	  free(line);
-	  line = NULL;
+		free(line);
+		exit(1);
 	}
-      else
-	{
-	  perror("error in read_line: getline");
-	  exit(EXIT_FAILURE);
-	}
-    }
-  else
-    {
-      line[strcspn(line, "\n")] = '\0';
-    }
-  return (line);
+
+	line[strcspn(line, "\n")] = '\0';
+	return (line);
 }
